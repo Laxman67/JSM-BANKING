f2b5e0aa31b4536f20f210d07e0abec52264bab1
Laxman67 <laxmanhenricks94@gmail.com>
2025-01-10 23:46:22 +0530

    initial  setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Inter,IBM_Plex_Serif } from 'next/font/google'
-import "./globals.css";
+ import "../globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const inter = Inter({subsets:['latin'],variable:'--font-inter'})
 const ibmPlexSerif=IBM_Plex_Serif({
@@ -34,12 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+   <main> 
+    {children}
+   </main>
   );
 }
